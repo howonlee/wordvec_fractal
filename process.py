ce284523af285946274947cf9b407a129336160f
Howon Lee <howon@howonlee.com>
2016-02-04 15:54:03 -0800

    won't work for normal stuff?

--- process.py
+++ process.py
@@ -14,30 +14,25 @@ def mat_to_points(mat):
     for x in xrange(mat.shape[0]):
         for y in xrange(mat.shape[1]):
             if mat[x,y] > 0:
-                points.append(
-                        (
-                            (float(x) / mat.shape[0]),
-                            (float(y) / mat.shape[1])
-                        )
-                )
+                points.append((float(x), float(y)))
     return points
 
-def box_hash(point, div):
+def box_hash(point, box_size):
     point = list(point)
-    point = map(lambda x: int(x * prod) // div, point)
+    point = map(lambda x: int(x) // box_size, point)
     return tuple(point)
 
-def num_boxes(points, div):
+def num_boxes(points, box_size):
     boxes = set()
     for point in points:
-        hsh = box_hash(point, div)
+        hsh = box_hash(point, box_size)
         boxes.add(hsh)
     return len(boxes)
 
 if __name__ == "__main__":
     test_pts = mat_to_points(sierpinski(order=9))
     boxes = []
-    for x in [8,16,32,64,128,256,512,1024]:
+    for x in [1,2,4,8,16,32,64]:
         boxes.append(num_boxes(test_pts, x))
-    plt.plot(boxes)
+    plt.loglog(boxes)
     plt.show()
