08e09f7ab5063eaf7e0d3ee999a78b4f64df1172
Howon Lee <howon@howonlee.com>
2016-02-05 12:37:33 -0800

    thinking about that process...

--- process.py
+++ process.py
@@ -12,8 +12,8 @@ def sierpinski(order=5):
     return arr
 
 def circle():
-    xx, yy = np.mgrid[:80, :80]
-    circle = ((xx - 40) ** 2 + (yy - 40) ** 2) < 150
+    xx, yy = np.mgrid[:200, :200]
+    circle = ((xx - 100) ** 2 + (yy - 100) ** 2) < 6400
     plt.imshow(circle)
     plt.show()
     return circle
@@ -24,6 +24,7 @@ def mat_to_points(mat):
         for y in xrange(mat.shape[1]):
             if mat[x,y] > 0:
                 points.append(np.array((float(x) / mat.shape[0], float(y) / mat.shape[1])))
+    print "num points: ", len(points)
     return points
 
 def sqrt_dist(fst, snd):
@@ -40,6 +41,7 @@ def correlation_integral(distances, epsilon):
     return float(len([1 for dist in distances if dist < epsilon])) / float(len(distances))
 
 def sierpinski_correlation():
+    # circle is a c&p of this, only it takes an obnoxiously long time
     sierpinski_points = mat_to_points(sierpinski())
     epsilons = np.logspace(-4, -1, num=10, base=2.0)
     print epsilons
@@ -55,12 +57,15 @@ def sierpinski_correlation():
     plt.plot(div_epsilons, np.polyval(p, div_epsilons), 'r-', label="least squares fit")
     plt.text(0.6, -0.7, r'$m = ' + str(p[0]) + "$", ha='center', va='center', fontsize=12)
     plt.legend(loc=1) # upper left
-    plt.title("Correlation dimension for Sierpinski Triangle") ########### !!!
+    plt.title("Correlation dimension for Sierpinski Triangle")
     plt.xlabel("log(epsilons / epsilons(0)), epsilons(0) chosen arbitrarily")
     plt.ylabel("log(correlation integrals)")
     plt.savefig("sierpinski")
 
+def word2vec_correlation(filename="vecs.txt"):
+    pass
+
 if __name__ == "__main__":
     matplotlib.rcParams['figure.figsize'] = (8,5)
     matplotlib.rcParams['savefig.dpi'] = 100
-    sierpinski_correlation()
+    word2vec_correlation()
