af0a55dded4890b45f84851c611e9fc34c68aa9e
Howon Lee <howon@howonlee.com>
2016-02-05 12:31:30 -0800

    caching is fun

--- process.py
+++ process.py
@@ -30,31 +30,37 @@ def sqrt_dist(fst, snd):
     # normal folks dist
     return np.sqrt(np.sum((fst - snd) ** 2))
 
-def correlation_integral(points, epsilon, dist_fn=sqrt_dist):
-    num_corrs = 0
-    for point1, point2 in itertools.product(points, points):
-        if dist_fn(point1, point2) < epsilon:
-            num_corrs += 1
-    return float(num_corrs) / float(len(points) ** 2)
+def memoize_distances(pts, dist_fn=sqrt_dist):
+    distances = []
+    for pt1, pt2 in itertools.product(pts, pts):
+        distances.append(dist_fn(pt1, pt2))
+    return distances
 
-if __name__ == "__main__":
-    matplotlib.rcParams['figure.figsize'] = (8,5)
-    matplotlib.rcParams['savefig.dpi'] = 100
+def correlation_integral(distances, epsilon):
+    return float(len([1 for dist in distances if dist < epsilon])) / float(len(distances))
+
+def sierpinski_correlation():
     sierpinski_points = mat_to_points(sierpinski())
     epsilons = np.logspace(-4, -1, num=10, base=2.0)
     print epsilons
     div_epsilons = np.array([np.log2(epsilon / epsilons[0]) for epsilon in epsilons])
     log_correlation_integrals = []
+    distances = memoize_distances(sierpinski_points)
     for epsilon in epsilons:
         print epsilon, " : starting..."
-        log_correlation_integrals.append(np.log2(correlation_integral(sierpinski_points, epsilon)))
+        log_correlation_integrals.append(np.log2(correlation_integral(distances, epsilon)))
         print "done"
     p = np.polyfit(div_epsilons, log_correlation_integrals, 1)
-    print p
     plt.plot(div_epsilons, log_correlation_integrals, "k.", label="correlation integrals")
     plt.plot(div_epsilons, np.polyval(p, div_epsilons), 'r-', label="least squares fit")
-    plt.legend(loc='best')
-    plt.title("Correlation dimension for sierpinski") ########### !!!
+    plt.text(0.6, -0.7, r'$m = ' + str(p[0]) + "$", ha='center', va='center', fontsize=12)
+    plt.legend(loc=1) # upper left
+    plt.title("Correlation dimension for Sierpinski Triangle") ########### !!!
     plt.xlabel("log(epsilons / epsilons(0)), epsilons(0) chosen arbitrarily")
     plt.ylabel("log(correlation integrals)")
-    plt.show()
+    plt.savefig("sierpinski")
+
+if __name__ == "__main__":
+    matplotlib.rcParams['figure.figsize'] = (8,5)
+    matplotlib.rcParams['savefig.dpi'] = 100
+    sierpinski_correlation()
